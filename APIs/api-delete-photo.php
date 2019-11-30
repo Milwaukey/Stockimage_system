1c899c96ed6e70cf3ee320a5479f21107a55b925
Louise Hauge Jessen <louisehaugejessen@Louises-MacBook-Pro.local>
2019-11-30 18:33:55 +0100

    numberOfPhotos In gallery on delete fixed

--- APIs/api-delete-photo.php
+++ APIs/api-delete-photo.php
@@ -16,14 +16,28 @@
     $sPhotoToBeDeleted = $_POST['tPhotoID'];
 
 
+        // UPDATE THE GALLERY  with the number of  images added
+        $query2 = "SELECT numberOfPhotos FROM tgalleries INNER JOIN tphotos ON tgalleries.galleryID = tphotos.galleryID WHERE photoID = " . $sPhotoToBeDeleted;
+        $result2 = mysqli_query($db, $query2);
+    
+        while($row = mysqli_fetch_array($result2)){
+    
+            $numberOfPhotos = $row['numberOfPhotos'];
+        
+            $newNumberOfPhotos = $numberOfPhotos-1;
+        
+            $query3 = "UPDATE tgalleries INNER JOIN tphotos ON tgalleries.galleryID = tphotos.galleryID SET tgalleries.numberOfPhotos = " . $newNumberOfPhotos ." WHERE photoID = " . $sPhotoToBeDeleted;
+    
+            mysqli_query($db, $query3);
+        
+        
+        }
+
+
     // DELETES THE PHOTO FROM THE DATABASE 
     $query = "DELETE FROM tphotos WHERE photoID = $sPhotoToBeDeleted";
 
 
     $result = mysqli_query($db, $query);
-
-
-
-
 
     echo sendResponse(1,'Photo Deleted!',__LINE__);
